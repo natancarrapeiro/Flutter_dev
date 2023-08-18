863b23d61a4c816035e7fc694d8e1158fd1d4995
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-08-18 12:52:07 -0300

    Booleano

--- init_Flutter/heelo_Flutter.dart
+++ init_Flutter/heelo_Flutter.dart
@@ -1,10 +1,21 @@
+import 'dart:ffi';
+
 void main() {
   print("hello ");
 
+//booleano
+  bool ligado = true;
+  var idadeBoll = 18;
+  bool maiorDeIdade = (idadeBoll >= 18);
+  print(ligado);
+  print(maiorDeIdade);
+  print("");
+//Number
   int idade = 15;
   int? semNumero; //quando nao a valor a variavel se torna NULL
   double salario = 1500.0;
-  var numeroGrande = 12e6;
+  var numeroGrande =
+      12e6; // ao colocar (e) se torna um numero exponecial acrecentando 0
 
   print("idade $idade salario de $salario");
   print(semNumero);
