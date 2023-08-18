a845bbad33e386e6a2541270a429e20ad4e0aca5
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-08-18 12:35:21 -0300

    Number

--- init_Flutter/heelo_Flutter.dart
+++ init_Flutter/heelo_Flutter.dart
@@ -1,3 +1,15 @@
 void main() {
   print("hello ");
+
+  int idade = 15;
+  int? semNumero; //quando nao a valor a variavel se torna NULL
+  double salario = 1500.0;
+  var numeroGrande = 12e6;
+
+  print("idade $idade salario de $salario");
+  print(semNumero);
+  semNumero = 14;
+  print(semNumero);
+  print(idade + salario);
+  print(numeroGrande);
 }
