51fc3010120b7ff21219f1a74dc0f7244a536314
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-08-20 10:04:27 -0300

    .

--- projeto/new_projeto/bin/class_Legumes.dart
+++ projeto/new_projeto/bin/class_Legumes.dart
@@ -1,6 +1,7 @@
 import 'class_Alimento.dart';
+import 'class_bolo.dart';
 
-class Legumes extends Alimento {
+class Legumes extends Alimento implements Bolo {
   bool isPrecisaCozinhar;
 
   Legumes(String nome, double peso, String cor, this.isPrecisaCozinhar)
@@ -11,6 +12,26 @@ class Legumes extends Alimento {
       print("O alimento $nome presisa ser cozinhado");
     } else {
       print("Não nessecita de cozimneto ");
+    
     }
+
+  }
+
+   @override
+  separarIngredientes() {
+    //ver os ingredientes
+    print("pegar o legumes");
+  }
+
+  @override
+  fazerMassa() {
+    //preparar os ingredientes e fazer a massa
+    print("prepare alegumes para o bolo");
+  }
+
+  @override
+ assar() {
+    //preparar a forma e verificar se esta cozido
+    print("coloque na forma e ligue o forno por alguns minutos");
   }
 }
