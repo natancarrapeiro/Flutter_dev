83e3f47eddbf9cab5bf7f56313dacf2a07b572d9
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-08-21 13:52:52 -0300

    remove segunda pasta dicas_dart

--- doc/dicas_dart/lib/entradaDados/entradaSaidaDados.dart
+++ doc/dicas_dart/lib/entradaDados/entradaSaidaDados.dart
@@ -6,3 +6,14 @@ suaIdade() {
   String? input = stdin.readLineSync();
   print("Sua idade é $input.");
 }
+
+qualSuaIdade(){
+  print("Qual a sua idade?");
+  String? input = stdin.readLineSync();
+  if (input != null) {
+    int idade = int.parse(input);
+    print("Ano que vem sua idade será ${idade + 1} anos.");
+  } else {
+    print("Não foi possível calcular o valor da idade");
+  }
+}
