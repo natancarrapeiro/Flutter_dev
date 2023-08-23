674a005633c83abff11d4d66a0f9cbb6b8d3601e
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-08-23 20:20:41 -0300

    exemplos tratamento null

--- dicas_dart/Exceptions_nullSafety/bank_dart/models/conta.dart
+++ dicas_dart/Exceptions_nullSafety/bank_dart/models/conta.dart
@@ -2,11 +2,13 @@ class Account {
   String name;
   double balance;
   bool isAuthenticated;
+  // ao poder ser null se não chamalo ele é null
+  DateTime? createdAt;
 
   Account(
       {required this.name,
       required this.balance,
-      required this.isAuthenticated})
+      required this.isAuthenticated,this.createdAt})
       : assert(name.isNotEmpty, "O nome não pode ser uma String Vazia"),
         assert(balance >= 0, "Não pode valor negativo");
 
--- dicas_dart/Exceptions_nullSafety/bank_dart/testNullSafety.dart
+++ dicas_dart/Exceptions_nullSafety/bank_dart/testNullSafety.dart
@@ -3,20 +3,35 @@ import 'dart:math';
 import 'models/conta.dart';
 
 testNullSafety (){
-  Account? myAccont;
+  Account? myAccont=Account(name: "natan", balance: 150, isAuthenticated: true);
   // Simulando uma comunicação externa
   Random rng=Random();
   int randomNumber =rng.nextInt(10);
   print(randomNumber);
   if(randomNumber<=5){
-    myAccont=Account(name: "natan", balance: 150, isAuthenticated: true);
+    myAccont.createdAt=DateTime.now();
   }
   print(myAccont.runtimeType);
+
+ //  teste foraçado nao funciona
  // print(myAccont.balance);
+  print(myAccont.createdAt);
+  // print(myAccont.createdAt.day);
+
+ //  teste  convertendo MA PRATICA
  //  print(myAccont!.balance);
- //  if(myAccont != null){
- //    print(myAccont.balance);
- //  }
-print(myAccont != null ? myAccont.name:"conta null");
-print(myAccont?.balance);
+ //  print(myAccont.createdAt!.day);
+
+ //  teste usando if
+  if(myAccont != null){
+    print(myAccont.balance);
+  }
+  if(myAccont.createdAt !=null){
+    print(myAccont.createdAt!.day);
+  }
+
+//   operado ternario
+// print(myAccont != null ? myAccont.name:"conta null");
+// // chamada segura
+// print(myAccont?.balance);
 }
